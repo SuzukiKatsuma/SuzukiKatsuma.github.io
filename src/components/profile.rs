f2d1d645edbc0f282f69c82c3134205dbfa32178
SuzukiKatsuma <71871253+SuzukiKatsuma@users.noreply.github.com>
2023-10-09 19:38:19 +0900

    update: twiiter icon to x icon

--- src/components/profile.rs
+++ src/components/profile.rs
@@ -11,11 +11,14 @@ pub fn profile(opacity :f32) -> Html {
 
           <p class="right">
             {"Contact me: "}
+            <a target="__blank" aria-label="Twitter" title="Twitter" href="https://twitter.com/_katsuma">
+              <i class="fa-brands fa-x-twitter"></i>
+            </a>
             <a target="__blank" aria-label="GitHub" title="GitHub" href="https://github.com/SuzukiKatsuma">
               <i class="fa-brands fa-github"></i>
             </a>
-            <a target="__blank" aria-label="Twitter" title="Twitter" href="https://twitter.com/SuzukiKatsuma">
-              <i class="fa-brands fa-twitter"></i>
+            <a target="__blank" aria-label="Zenn" title="Zenn" href="https://zenn.dev/suzuki_katsuma">
+              <i class="fa-solid fa-paperclip"></i>
             </a>
           </p>
 
