8e8eed95320c1f6e15b645ba61e6f8c5f7f86a91
SuzukiKatsuma <71871253+SuzukiKatsuma@users.noreply.github.com>
2025-03-19 01:40:11 +0900

    feat: add icon to profile component

--- src/components/profile.rs
+++ src/components/profile.rs
@@ -1,34 +1,37 @@
 use yew::prelude::*;
 
-pub fn profile(opacity :f32) -> Html {
+pub fn profile(opacity: f32) -> Html {
     html! {
-      <>
-        <div class="card profile">
-          <p>{"Hello, I'm"}</p>
+      <div class="card profile">
+        <div class="introduction">
+          <figure class="icon">
+            <img src="img/icon.png" alt="icon" />
+          </figure>
+
+          <p>{"Hello, I'm"}
           <h1>{"Suzuki Katsuma"}</h1>
-
-          <br/>
-
-          <p class="right">
-            {"Contact me: "}
-            <a target="__blank" aria-label="Twitter" title="Twitter" href="https://twitter.com/_katsuma">
-              <i class="fa-brands fa-x-twitter"></i>
-            </a>
-            <a target="__blank" aria-label="GitHub" title="GitHub" href="https://github.com/SuzukiKatsuma">
-              <i class="fa-brands fa-github"></i>
-            </a>
-            <a target="__blank" aria-label="Zenn" title="Zenn" href="https://zenn.dev/suzuki_katsuma">
-              <i class="fa-solid fa-paperclip"></i>
-            </a>
           </p>
-
-          <style>
-            {".profile {"}
-            {"opacity: "}{opacity}{";"}
-            if 0.1 > opacity {{"pointer-events: none;"}}
-            {"}"}
-          </style>
         </div>
-      </>
+
+        <p class="right">
+          {"Contact me: "}
+          <a target="__blank" aria-label="Twitter" title="Twitter" href="https://twitter.com/_katsuma">
+            <i class="fa-brands fa-x-twitter"></i>
+          </a>
+          <a target="__blank" aria-label="GitHub" title="GitHub" href="https://github.com/SuzukiKatsuma">
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a target="__blank" aria-label="Zenn" title="Zenn" href="https://zenn.dev/suzuki_katsuma">
+            <i class="fa-solid fa-paperclip"></i>
+          </a>
+        </p>
+
+        <style>
+          {".profile {"}
+          {"opacity: "}{opacity}{";"}
+          if 0.1 > opacity {{"pointer-events: none;"}}
+          {"}"}
+        </style>
+      </div>
     }
 }
